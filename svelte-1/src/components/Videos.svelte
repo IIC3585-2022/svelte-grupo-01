9fe6ab52283c91d79da2e1dee65c227316ada4c2
Joaquín <jncaceres@uc.cl>
2022-06-05 23:18:01 -0400

    add async await

--- svelte-1/src/components/Videos.svelte
+++ svelte-1/src/components/Videos.svelte
@@ -2,21 +2,26 @@
   import YoutubeVideo from "./YoutubeVideo.svelte";
   const urlParams = new URLSearchParams(window.location.search);
   const searchQuery = urlParams.get("search");
-  let videos = [];
-  fetch(
-    `https://www.googleapis.com/youtube/v3/search?q=${searchQuery}&part=snippet&maxResults=50&key=AIzaSyDIq4ZGvS8Z3FXABUHvBFvSAAHXMALeTmA`
-  )
-    .then((data) => data.json())
-    .then((vids) => {
-      videos = [...vids.items];
-    });
+  async function searchYoutube(searchQuery: string) {
+    let videos = [];
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?q=${searchQuery}&part=snippet&maxResults=50&key=AIzaSyDIq4ZGvS8Z3FXABUHvBFvSAAHXMALeTmA`
+    );
+    const data = await response.json();
+    videos = data.items;
+    return videos;
+  }
 </script>
 
 <div class="w-full">
   <h1 class="title">Resultados de busqueda</h1>
-  {#each videos as video (video)}
-    <YoutubeVideo {video} />
-  {/each}
+  {#await searchYoutube(searchQuery)}
+    <div class="searching-text">Buscando...</div>
+  {:then videos}
+    {#each videos as video (video)}
+      <YoutubeVideo {video} />
+    {/each}
+  {/await}
 </div>
 
 <style>
@@ -30,5 +35,8 @@
     width: 100%;
     text-align: center;
     display: block;
+  }
+  div.searching-text {
+    color: #ffffff;
   }
 </style>
